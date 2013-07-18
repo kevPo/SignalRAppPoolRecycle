14e2575ecae346f38777be00c3d5d0d129e0cd5e
Kevin Poston <kevposton@gmail.com>
2013-07-18 12:30:13 -0400

    initial load

---
+++ SignalRAppPoolRecycle/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Timers;
+using Microsoft.AspNet.SignalR.Client;
+using Microsoft.AspNet.SignalR.Client.Hubs;
+
+namespace SignalRAppPoolRecycle
+{
+    class Program
+    {
+        private static HubConnection _hubConnection;
+        private static IHubProxy _conversationProxy;
+        private static readonly Timer Timer = new Timer(10000);
+        public static void Main()
+        {
+            Timer.Elapsed += Elapsed;
+            Timer.Start();
+            _hubConnection = new HubConnection("http://localhost:50025");
+            _conversationProxy = _hubConnection.CreateHubProxy("RecycleHub");
+            _conversationProxy.On("Broadcast", message => Console.WriteLine(message));
+            _conversationProxy.On("Online", message => Console.WriteLine(message));
+
+            _hubConnection.Closed += HubConnectionOnClosed;
+            _hubConnection.StateChanged += HubConnectionStateChanged;
+            ConnectToHub();
+            Console.ReadLine();
+        }
+
+        private static void Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (_hubConnection.State == ConnectionState.Connected)
+            {
+                _conversationProxy.Invoke("Relay");
+            }
+        }
+
+        private static void HubConnectionStateChanged(StateChange stateChange)
+        {
+            Console.WriteLine(@"{0} - Client state is {1}", DateTime.Now, _hubConnection.State);
+            Console.WriteLine(@"Connection ID is {0}", _hubConnection.ConnectionId);
+
+            if (_hubConnection.State == ConnectionState.Connected)
+            {
+                Console.WriteLine(@"{0} - UserOnline sent to hub", DateTime.Now);
+                _conversationProxy.Invoke("UserOnline").Wait();
+            }
+        }
+
+        private static void HubConnectionOnClosed()
+        {
+            Console.WriteLine(@"{0} - Hub connection closed", DateTime.Now);
+            ConnectToHub();
+        }
+
+        private static void ConnectToHub()
+        {
+            _hubConnection.Start().Wait();
+        }
+
+
+    }
+}
---
+++ SignalRHubTester/Hubs/RecycleHub.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRHubTester.Hubs
+{
+    public class RecycleHub : Hub
+    {
+        private static readonly Dictionary<string, int> OnlineUsers = new Dictionary<string, int>();
+        private static int _index;
+
+        public override System.Threading.Tasks.Task OnDisconnected()
+        {
+            OnlineUsers.Remove(Context.ConnectionId);
+            return base.OnDisconnected();
+        }
+
+        public void UserOnline()
+        {
+            var index = _index++;
+            OnlineUsers.Add(Context.ConnectionId, index);
+            Clients.Client(Context.ConnectionId).Online("You are currently online as user " + index);    
+        }
+
+        public void Relay()
+        {
+            Clients.Client(Context.ConnectionId).Broadcast(DateTime.Now + " - You have successfully pinged the hub.");
+            Clients.AllExcept(Context.ConnectionId).Broadcast(DateTime.Now + " - User" + OnlineUsers[Context.ConnectionId] + " has pinged the hub.");
+        }
+    }
+}
